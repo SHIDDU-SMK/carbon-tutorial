53b6729ea15ad3dbbadbdf46d50f3bae10665878
shiddugmail <shiddu@gmail.com>
2021-01-23 17:29:20 +0530

    feat(tutorial): complete step 1

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const LandingPage = () => {
+  return (
+    <div className="bg-center" style={{ paddingTop: '5rem' }}>
+      LANDING PAGE
+    </div>
+  );
+};
+export default LandingPage;
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const RepoPage = () => {
+  return (
+    <div className="bg-center" style={{ paddingTop: '5rem' }}>
+      REPO PAGE
+    </div>
+  );
+};
+export default RepoPage;
